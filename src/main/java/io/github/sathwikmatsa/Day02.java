06e5af69c985fcd4b50de8d9e7b2d3b82fde700f
Sathwik Matsa <sathwikmatsa@gmail.com>
2024-12-02 13:32:31 +0530

    day02 part 2

--- src/main/java/io/github/sathwikmatsa/Day02.java
+++ src/main/java/io/github/sathwikmatsa/Day02.java
@@ -5,6 +5,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 class Day02 {
@@ -41,8 +42,37 @@ class Day02 {
         return reports.stream().filter(Day02::isReportSafe).count();
     }
 
+    static boolean isReportSafeV2(List<Integer> report) {
+        if (isReportSafe(report)) {
+            return true;
+        }
+
+        boolean increasing = report.get(0) < report.get(1);
+        int fault_index = IntStream.range(1, report.size()).filter(i -> {
+            Integer left = report.get(i - 1);
+            Integer right = report.get(i);
+            int diff = Math.abs(left - right);
+            boolean safe = (increasing ? right > left : right < left) && diff >= 1 && diff <= 3;
+            return !safe;
+        }).findFirst().getAsInt();
+
+        List<Integer> report_without_fi = IntStream.range(0, report.size()).filter(i -> i != fault_index)
+                .mapToObj(report::get).collect(Collectors.toList());
+        List<Integer> report_without_fip = IntStream.range(0, report.size()).filter(i -> i != fault_index - 1)
+                .mapToObj(report::get).collect(Collectors.toList());
+        List<Integer> report_without_first = IntStream.range(0, report.size()).filter(i -> i != 0)
+                .mapToObj(report::get).collect(Collectors.toList());
+        return isReportSafe(report_without_fi) || isReportSafe(report_without_fip)
+                || isReportSafe(report_without_first);
+    }
+
+    static long countSafeReportsV2(List<List<Integer>> reports) {
+        return reports.stream().filter(Day02::isReportSafeV2).count();
+    }
+
     public static void main(String[] args) throws IOException {
         List<List<Integer>> reports = parseInput("input/day02_part1.txt");
         System.out.println("Part1: " + countSafeReports(reports));
+        System.out.println("Part2: " + countSafeReportsV2(reports));
     }
 }
--- src/test/java/io/github/sathwikmatsa/Day02Test.java
+++ src/test/java/io/github/sathwikmatsa/Day02Test.java
@@ -36,4 +36,19 @@ public class Day02Test {
         List<List<Integer>> reports = Day02.parseInput("input/day02_test_levels.txt");
         assertEquals(2, Day02.countSafeReports(reports));
     }
+
+    @Test
+    public void isReportSafeV2() {
+        assertEquals(true, Day02.isReportSafeV2(Arrays.asList(7, 6, 4, 2, 1)));
+        assertEquals(false, Day02.isReportSafeV2(Arrays.asList(1, 2, 7, 8, 9)));
+        assertEquals(false, Day02.isReportSafeV2(Arrays.asList(9, 7, 6, 2, 1)));
+        assertEquals(true, Day02.isReportSafeV2(Arrays.asList(1, 3, 2, 4, 5)));
+        assertEquals(true, Day02.isReportSafeV2(Arrays.asList(8, 6, 4, 4, 1)));
+        assertEquals(true, Day02.isReportSafeV2(Arrays.asList(1, 3, 6, 7, 9)));
+
+        assertEquals(true, Day02.isReportSafeV2(Arrays.asList(9, 8, 9, 10)));
+        assertEquals(true, Day02.isReportSafeV2(Arrays.asList(7, 9, 8, 9, 10)));
+        assertEquals(true, Day02.isReportSafeV2(Arrays.asList(9, 10, 8, 7)));
+
+    }
 }
