98e85d6301127b49454a4e1043161194d467bfbb
Sathwik Matsa <sathwikmatsa@gmail.com>
2024-12-06 17:42:46 +0530

    day06 part2

--- src/main/java/io/github/sathwikmatsa/Day06.java
+++ src/main/java/io/github/sathwikmatsa/Day06.java
@@ -4,9 +4,11 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.AbstractMap.SimpleEntry;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 enum Direction {
     UP(-1, 0),
@@ -35,7 +37,7 @@ enum Direction {
     }
 }
 
-class GuardPosition {
+class GuardPosition implements Cloneable {
     public GuardPosition(int row, int col, Direction direction) {
         this.row = row;
         this.col = col;
@@ -46,13 +48,13 @@ class GuardPosition {
         return new SimpleEntry<Integer, Integer>(row, col);
     }
 
-    public boolean advance(String[] patrolArea) {
+    public boolean advance(Character[][] patrolArea) {
         while (true) {
             int nr = row + direction.getDr();
             int nc = col + direction.getDc();
 
-            if (nr < patrolArea.length && nc < patrolArea[0].length()) {
-                if (patrolArea[nr].charAt(nc) == '#') {
+            if (nr >= 0 && nr < patrolArea.length && nc >= 0 && nc < patrolArea[0].length) {
+                if (patrolArea[nr][nc] == '#') {
                     direction = direction.getRight();
                 } else {
                     this.row = nr;
@@ -68,26 +70,90 @@ class GuardPosition {
     private int row;
     private int col;
     private Direction direction;
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + Integer.hashCode(row);
+        result = prime * result + Integer.hashCode(col);
+        result = prime * result + direction.hashCode();
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (!(obj instanceof GuardPosition))
+            return false;
+        GuardPosition other = (GuardPosition) obj;
+        return row == other.row && col == other.col && direction == other.direction;
+    }
+
+    @Override
+    public GuardPosition clone() {
+        try {
+            return (GuardPosition) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new AssertionError();
+        }
+    }
 }
 
 class Day06 {
-    static String[] parseInput(String filename) throws IOException {
-        return Files.readString(Paths.get(filename)).split("\n");
+    static SimpleEntry<GuardPosition, Character[][]> parseInput(String filename) throws IOException {
+        Character[][] area = Stream.of(Files.readString(Paths.get(filename)).split("\n"))
+                .map(line -> line.chars().mapToObj(c -> (char) c).toArray(Character[]::new))
+                .toArray(Character[][]::new);
+        GuardPosition guard = IntStream.range(0, area.length)
+                .filter(r -> Arrays.stream(area[r]).anyMatch(c -> c == '^'))
+                .mapToObj(r -> new GuardPosition(r, Arrays.asList(area[r]).indexOf('^'), Direction.UP)).findFirst()
+                .get();
+        return new SimpleEntry<>(guard, area);
     }
 
-    static int patrolPositions(String[] area) {
+    static Set<SimpleEntry<Integer, Integer>> patrolPositions(Character[][] area, GuardPosition guardOrig) {
+        GuardPosition guard = guardOrig.clone();
         Set<SimpleEntry<Integer, Integer>> posSet = new HashSet<>();
-        GuardPosition guard = IntStream.range(0, area.length).filter(r -> area[r].contains("^"))
-                .mapToObj(r -> new GuardPosition(r, area[r].indexOf('^'), Direction.UP)).findFirst().get();
         posSet.add(guard.getPos());
         while (guard.advance(area)) {
             posSet.add(guard.getPos());
         }
-        return posSet.size();
+        return posSet;
+    }
+
+    static boolean placeObstacleAndCheckLoop(Character[][] area, GuardPosition guardOrig,
+            SimpleEntry<Integer, Integer> obstaclePos) {
+        area[obstaclePos.getKey()][obstaclePos.getValue()] = '#';
+        Set<GuardPosition> posSet = new HashSet<>();
+        GuardPosition guard = guardOrig.clone();
+        boolean loop = false;
+        while (guard.advance(area)) {
+            if (posSet.contains(guard)) {
+                loop = true;
+                break;
+            }
+            posSet.add(guard.clone());
+        }
+        area[obstaclePos.getKey()][obstaclePos.getValue()] = '.';
+        return loop;
+    }
+
+    static long loopingObstructions(Character[][] area, GuardPosition guard,
+            Set<SimpleEntry<Integer, Integer>> allowList) {
+        return allowList.stream().filter(pos -> pos != guard.getPos() && placeObstacleAndCheckLoop(area, guard, pos))
+                .count();
     }
 
     public static void main(String[] args) throws IOException {
-        String[] input = parseInput("input/day06_part1.txt");
-        System.out.println("part1: " + patrolPositions(input));
+        SimpleEntry<GuardPosition, Character[][]> input = parseInput("input/day06_part1.txt");
+        GuardPosition guard = input.getKey();
+        Character[][] area = input.getValue();
+
+        Set<SimpleEntry<Integer, Integer>> positions = patrolPositions(area, guard);
+
+        System.out.println("part1: " + positions.size());
+        System.out.println("part2: " + loopingObstructions(area, guard, positions));
     }
 }
--- src/test/java/io/github/sathwikmatsa/Day06Test.java
+++ src/test/java/io/github/sathwikmatsa/Day06Test.java
@@ -1,15 +1,23 @@
 package io.github.sathwikmatsa;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.IOException;
+import java.util.AbstractMap.SimpleEntry;
+import java.util.Set;
 
 import org.junit.jupiter.api.Test;
 
 public class Day06Test {
     @Test
     public void parseInput() throws IOException {
-        assertEquals(10, Day06.parseInput("input/day06_test_part1.txt").length);
+        SimpleEntry<GuardPosition, Character[][]> input = Day06.parseInput("input/day06_test_part1.txt");
+        GuardPosition guard = input.getKey();
+        Character[][] area = input.getValue();
+        assertEquals(10, area.length);
+        assertEquals(new GuardPosition(6, 4, Direction.UP), guard);
     }
 
     @Test
@@ -22,7 +30,37 @@ public class Day06Test {
 
     @Test
     public void patrolPositions() throws IOException {
-        String[] input = Day06.parseInput("input/day06_test_part1.txt");
-        assertEquals(41, Day06.patrolPositions(input));
+        SimpleEntry<GuardPosition, Character[][]> input = Day06.parseInput("input/day06_test_part1.txt");
+        GuardPosition guard = input.getKey();
+        Character[][] area = input.getValue();
+        assertEquals(41, Day06.patrolPositions(area, guard).size());
+    }
+
+    @Test
+    public void placeObstacleAndCheckLoop() throws IOException {
+        SimpleEntry<GuardPosition, Character[][]> input = Day06.parseInput("input/day06_test_part1.txt");
+        GuardPosition guard = input.getKey();
+        Character[][] area = input.getValue();
+
+        assertTrue(Day06.placeObstacleAndCheckLoop(area, guard, new SimpleEntry<Integer, Integer>(6, 3)));
+        assertTrue(Day06.placeObstacleAndCheckLoop(area, guard, new SimpleEntry<Integer, Integer>(7, 6)));
+        assertTrue(Day06.placeObstacleAndCheckLoop(area, guard, new SimpleEntry<Integer, Integer>(7, 7)));
+        assertTrue(Day06.placeObstacleAndCheckLoop(area, guard, new SimpleEntry<Integer, Integer>(8, 1)));
+        assertTrue(Day06.placeObstacleAndCheckLoop(area, guard, new SimpleEntry<Integer, Integer>(8, 3)));
+        assertTrue(Day06.placeObstacleAndCheckLoop(area, guard, new SimpleEntry<Integer, Integer>(9, 7)));
+
+        assertFalse(Day06.placeObstacleAndCheckLoop(area, guard, new SimpleEntry<Integer, Integer>(0, 0)));
+    }
+
+    @Test
+    public void loopingObstructions() throws IOException {
+        SimpleEntry<GuardPosition, Character[][]> input = Day06.parseInput("input/day06_test_part1.txt");
+        GuardPosition guard = input.getKey();
+        Character[][] area = input.getValue();
+
+        Set<SimpleEntry<Integer, Integer>> positions = Day06.patrolPositions(area, guard.clone());
+
+        assertEquals(6, Day06.loopingObstructions(area, guard, positions));
+
     }
 }
